ee1cbdd8055c5508d48d338e54d04a5709c9a438
Sanakara <jamalsmith14@mail.com>
2016-07-13 16:57:47 +0200

    Login, username and password checking season2

--- app/src/main/java/database/thing/GooglePlacesActivity.java
+++ app/src/main/java/database/thing/GooglePlacesActivity.java
@@ -1,29 +1,48 @@
 package database.thing;
 
+import android.Manifest;
 import android.app.Activity;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.location.Location;
+import android.location.LocationListener;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.identity.intents.AddressConstants;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.MapView;
 
-public class GooglePlacesActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class GooglePlacesActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
+    private static final long MIN_TIME_BW_UPDATES = 1000;
+    private static final int REQUEST_PERMISSION_ACCESS_LOCATION = 0;
+
 
     GoogleApiClient googleApiClient;
     MapView places;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_places);
 
-        places = (MapView)findViewById(R.id.places);
+        places = (MapView) findViewById(R.id.places);
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Places.GEO_DATA_API)
                 .addApi(Places.PLACE_DETECTION_API)
@@ -35,8 +54,20 @@ public class GooglePlacesActivity extends FragmentActivity implements GoogleApiC
     @Override
     protected void onStart() {
         super.onStart();
+
         googleApiClient.connect();
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_PERMISSION_ACCESS_LOCATION);
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
     }
+
 
     @Override
     protected void onStop() {
@@ -51,4 +82,23 @@ public class GooglePlacesActivity extends FragmentActivity implements GoogleApiC
                 Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e("LOCATION", "blop");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
